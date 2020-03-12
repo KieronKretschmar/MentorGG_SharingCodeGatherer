107435d3c38a38708548480a21bd65cf74b5042c
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2020-03-12 17:48:41 +0100

    Add migration adding analyzedQuality into db

---
+++ Database/Migrations/20200312160630_add analyzer quality.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Database.Migrations
+{
+    public partial class addanalyzerquality : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "AnalyzedQuality",
+                table: "Matches",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AnalyzedQuality",
+                table: "Matches");
+        }
+    }
+}
